b518bf0de1077001d6d2df7f1d5cb3052ceea802
DhrunitSixberries <dhrunit@sixberries.com>
2021-12-22 14:37:36 +0530

    Added basic error handling and completed login, signup feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
+import { body } from "express-validator";
 const router = express.Router();
-import HttpError from "../models/http-error.js";
-import { registerUser } from "../controllers/userController.js";
+import { authUser, registerUser } from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
-router.route("/").post(registerUser);
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  registerUser
+);
+router.post(
+  "/login",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  authUser
+);
 
 export default router;
