0589cd94bee73772a9827849491fa15c51ea7320
DhrunitSixberries <dhrunit@sixberries.com>
2021-12-28 21:34:17 +0530

    Complete Wallets Routes and controllers

--- backend/controllers/walletController.js
+++ backend/controllers/walletController.js
@@ -15,7 +15,6 @@ const addWallet = async (req, res, next) => {
     );
   }
   const {
-    user,
     name,
     currency,
     balance,
@@ -37,7 +36,7 @@ const addWallet = async (req, res, next) => {
   }
   // create a wallet
   const wallet = await Wallet.create({
-    user,
+    user: req.user._id,
     name,
     currency,
     balance,
@@ -55,7 +54,7 @@ const addWallet = async (req, res, next) => {
 
   // update the user Model with wallet id
   if (isActiveWallet) {
-    let userLinked = await User.findByIdAndUpdate(user, {
+    let userLinked = await User.findByIdAndUpdate(req.user._id, {
       activeWallet: wallet._id,
     });
     if (!userLinked) {
@@ -69,4 +68,146 @@ const addWallet = async (req, res, next) => {
   });
 };
 
-export { addWallet };
+// @desc    Auth user & get token
+// @route   POST /api/wallet/editWallet
+// @access  Private
+const editWallet = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid input or data missing in request body", 422)
+    );
+  }
+  const {
+    walletId,
+    name,
+    currency,
+    balance,
+    hasBudget,
+    budgetAmount,
+    resetBalance,
+    resetPeriod,
+    isActiveWallet,
+  } = req.body;
+
+  //   Calculate reset time
+  let resetTime;
+  if (resetPeriod.toLowerCase() === "monthly") {
+    resetTime = moment().add(1, "months").format();
+  } else if (resetPeriod.toLowerCase() === "weekly") {
+    resetTime = moment().add(1, "weeks").format();
+  } else {
+    resetTime = moment().add(1, "years").format();
+  }
+  // update the wallet
+  const wallet = await Wallet.findByIdAndUpdate(
+    walletId,
+    {
+      user: req.user._id,
+      name,
+      currency,
+      balance,
+      hasBudget,
+      budgetAmount,
+      resetBalance,
+      resetPeriod,
+      isActiveWallet,
+      activeDate: moment().format(),
+      resetTime,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!wallet) {
+    return next(new HttpError("Wallet update failed", 400));
+  }
+
+  // update the user Model with wallet id
+  if (isActiveWallet) {
+    let userLinked = await User.findByIdAndUpdate(req.user._id, {
+      activeWallet: wallet._id,
+    });
+    if (!userLinked) {
+      return next(new HttpError("Linking the wallet with user failed", 400));
+    }
+  }
+  res.status(201).send({
+    success: true,
+    data: { wallet },
+    message: "Wallet Updated successfully",
+  });
+};
+
+// @desc    Auth user & get token
+// @route   POST /api/wallet/deleteWallet
+// @access  Private
+const deleteWallet = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid input or data missing in request body", 422)
+    );
+  }
+  const { walletId } = req.body;
+
+  // delete the wallet
+  const wallet = await Wallet.findByIdAndDelete(walletId);
+  if (!wallet) {
+    return next(new HttpError("Wallet delete failed", 400));
+  }
+  res.status(201).send({
+    success: true,
+    message: "Wallet deleted successfully",
+  });
+};
+
+// @desc    Auth user & get token
+// @route   POST /api/wallet/getWallets
+// @access  Private
+const getWalletsForUser = async (req, res, next) => {
+  let limit = 10;
+  const { _id: userId } = req.user;
+  let { page, searchString } = req.query;
+  if (!page || !page.trim()) {
+    return next(new HttpError("Page missing in query params", 400));
+  }
+  if (searchString.trim()) {
+    let toSkip = parseInt(page - 1) * limit;
+    const wallets = await Wallet.find({
+      user: userId,
+      name: { $regex: searchString, $options: "i" },
+    })
+      .skip(toSkip)
+      .limit(limit)
+      .select("name");
+    if (!wallets) {
+      return next(
+        new HttpError("An error occured while fetching wallets", 400)
+      );
+    }
+    res.status(201).send({
+      success: true,
+      data: wallets,
+      message: "Wallet fetched successfully",
+    });
+  } else {
+    let toSkip = parseInt(page - 1) * limit;
+    const wallets = await Wallet.find({ user: userId })
+      .skip(toSkip)
+      .limit(limit)
+      .select("name");
+    if (!wallets) {
+      return next(
+        new HttpError("An error occured while fetching wallets", 400)
+      );
+    }
+    res.status(201).send({
+      success: true,
+      data: wallets,
+      message: "Wallet fetched successfully",
+    });
+  }
+};
+
+export { addWallet, editWallet, deleteWallet, getWalletsForUser };
--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { body } from "express-validator";
-import { addWallet } from "../controllers/walletController.js";
+import {
+  addWallet,
+  deleteWallet,
+  editWallet,
+  getWalletsForUser,
+} from "../controllers/walletController.js";
 const router = express.Router();
 import { protect } from "../middlewares/authMiddleware.js";
 
 router.post(
   "/addWallet",
-  body("user").isMongoId(),
   body("name").isLength({ min: 6 }),
   body("currency").notEmpty(),
   body("balance").notEmpty().isInt(),
@@ -18,5 +22,29 @@ router.post(
   protect,
   addWallet
 );
+
+router.put(
+  "/editWallet",
+  body("walletId").isMongoId(),
+  body("name").isLength({ min: 6 }),
+  body("currency").notEmpty(),
+  body("balance").notEmpty().isInt(),
+  body("hasBudget").notEmpty().isBoolean(),
+  body("budgetAmount").notEmpty().isInt(),
+  body("resetBalance").notEmpty().isBoolean(),
+  body("resetPeriod").notEmpty(),
+  body("isActiveWallet").notEmpty().isBoolean(),
+  protect,
+  editWallet
+);
+
+router.delete(
+  "/deleteWallet",
+  body("walletId").isMongoId(),
+  protect,
+  deleteWallet
+);
+
+router.get("/getWallets", protect, getWalletsForUser);
 
 export default router;
