7437d8350ed8d01093988f409c9db40827d87630
DhrunitSixberries <dhrunit@sixberries.com>
2022-01-01 13:07:30 +0530

    Added try catch and completed dashboard route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,29 +8,33 @@ import generateToken from "../utils/generateToken.js";
 // @route   POST /api/users/login
 // @access  Public
 const authUser = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new HttpError("Invalid input or data missing in request body", 422)
-    );
-  }
-  const { email, password } = req.body;
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(
+        new HttpError("Invalid input or data missing in request body", 422)
+      );
+    }
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    res.send({
-      success: true,
-      data: {
-        _id: user._id,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      },
-      message: "Login successful",
-    });
-  } else {
-    return next(new HttpError("Invalid email or password", 401));
+    if (user && (await user.matchPassword(password))) {
+      res.send({
+        success: true,
+        data: {
+          _id: user._id,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        },
+        message: "Login successful",
+      });
+    } else {
+      return next(new HttpError("Invalid email or password", 401));
+    }
+  } catch (error) {
+    return next(new HttpError("Internal Server error", 500));
   }
 };
 
@@ -38,37 +42,41 @@ const authUser = async (req, res, next) => {
 // @route   POST /api/users/changePassword
 // @access  Private
 const changePassword = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new HttpError("Invalid input or data missing in request body", 422)
-    );
-  }
-  let { password } = req.body;
-  const user = await User.findOne({ email: req.user.email });
-  if (!user) {
-    return next(new HttpError("Invalid user Id", 401));
-  }
-  if (await user.matchPassword(password, user.password)) {
-    res.send({
-      success: false,
-      message: "Password is same",
-    });
-  } else {
-    const salt = await bcrypt.genSalt(10);
-    password = await bcrypt.hash(password, salt);
-    user.password = password;
-    let result = await User.findByIdAndUpdate(user._id, { ...user });
-    if (!result) {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
       return next(
-        new HttpError("Something went wrong while updating the password", 400)
+        new HttpError("Invalid input or data missing in request body", 422)
       );
-    } else {
-      res.send({
-        success: true,
-        message: "Password changed successfully",
-      });
     }
+    let { password } = req.body;
+    const user = await User.findOne({ email: req.user.email });
+    if (!user) {
+      return next(new HttpError("Invalid user Id", 401));
+    }
+    if (await user.matchPassword(password, user.password)) {
+      res.send({
+        success: false,
+        message: "Password is same",
+      });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      password = await bcrypt.hash(password, salt);
+      user.password = password;
+      let result = await User.findByIdAndUpdate(user._id, { ...user });
+      if (!result) {
+        return next(
+          new HttpError("Something went wrong while updating the password", 400)
+        );
+      } else {
+        res.send({
+          success: true,
+          message: "Password changed successfully",
+        });
+      }
+    }
+  } catch (error) {
+    return next(new HttpError("Internal Server error", 500));
   }
 };
 
@@ -76,42 +84,46 @@ const changePassword = async (req, res, next) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new HttpError("Invalid input or data missing in request body", 422)
-    );
-  }
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(
+        new HttpError("Invalid input or data missing in request body", 422)
+      );
+    }
 
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    return next(new HttpError("User already exists", 400));
-  }
-  let pictureUrl = `https://avatars.dicebear.com/api/miniavs/${Math.random()}.svg`;
-  const user = await User.create({
-    email,
-    password,
-    pictureUrl,
-  });
-
-  if (user) {
-    res.status(201).json({
-      success: true,
-      data: {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        pictureUrl: user.pictureUrl,
-        token: generateToken(user._id),
-      },
-      message: "User registered successfully",
+    if (userExists) {
+      return next(new HttpError("User already exists", 400));
+    }
+    let pictureUrl = `https://avatars.dicebear.com/api/miniavs/${Math.random()}.svg`;
+    const user = await User.create({
+      email,
+      password,
+      pictureUrl,
     });
-  } else {
-    res.status(400);
-    return next(new HttpError("Invalid user data", 400));
+
+    if (user) {
+      res.status(201).json({
+        success: true,
+        data: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          pictureUrl: user.pictureUrl,
+          token: generateToken(user._id),
+        },
+        message: "User registered successfully",
+      });
+    } else {
+      res.status(400);
+      return next(new HttpError("Invalid user data", 400));
+    }
+  } catch (error) {
+    return next(new HttpError("Internal Server error", 500));
   }
 };
 
--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { body } from "express-validator";
 import { getDashboardDetails } from "../controllers/dashboardController.js";
 const router = express.Router();
 import { protect } from "../middlewares/authMiddleware.js";
 
-router.get("/getDashboardDetails", protect, getDashboardDetails);
+router.get("/getDashboardDetails/:month", protect, getDashboardDetails);
 
 export default router;
