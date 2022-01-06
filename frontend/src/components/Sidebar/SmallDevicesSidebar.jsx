1f3a357c7f40e881f300af2dbdbbf24b21052498
DhrunitSixberries <dhrunit@sixberries.com>
2022-01-06 08:11:04 +0530

    Made sidebar responsive

---
+++ frontend/src/components/Sidebar/SmallDevicesSidebar.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import Form from "../Form";
+import sidebarArray from "./helpers";
+import {
+  BrandDrawerMobile,
+  StyledAppBarMobile,
+  StyledDrawerMobile,
+} from "./style";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const SmallDevicesSidebar = ({
+  collapsed,
+  setCollapsed,
+  search,
+  setSearch,
+  userDetails,
+  handleClick,
+  menuOpen,
+  handleClose,
+  anchorEl,
+  logout,
+}) => {
+  return (
+    <div>
+      <StyledDrawerMobile
+        variant="temporary"
+        sx={{
+          display: { md: "block", sm: "block", xs: "block" },
+        }}
+        open={!collapsed}
+        collapsed={collapsed}
+        onClose={() => setCollapsed(true)}
+      >
+        <div style={{ marginTop: "1rem" }}>
+          {!collapsed && <BrandDrawerMobile />}
+        </div>
+        <List>
+          {sidebarArray.map((element, index) => (
+            <ListItem button key={element.name} sx={{ height: "3.5rem" }}>
+              <Link
+                to={element.to}
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <ListItemIcon
+                    sx={{
+                      color: "#fff",
+                      transition: "all 0.2s",
+                      marginLeft: !collapsed && "0.3rem",
+                      minWidth: "45px",
+                    }}
+                  >
+                    {element.icon}
+                  </ListItemIcon>
+                  {!collapsed && <ListItemText primary={element.name} />}
+                </div>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+      </StyledDrawerMobile>
+      <StyledAppBarMobile position="static" elevation={0} collapsed={collapsed}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-end",
+            height: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <IconButton onClick={() => setCollapsed(!collapsed)}>
+            {collapsed ? (
+              <KeyboardDoubleArrowRightIcon />
+            ) : (
+              <KeyboardDoubleArrowLeftIcon />
+            )}
+          </IconButton>
+          <div
+            style={{
+              display: "flex",
+              marginRight: "1rem",
+              alignItems: "center",
+            }}
+          >
+            <Form
+              type={"search"}
+              placeHolder={"Search"}
+              value={search}
+              onChange={(evt) => setSearch(evt.target.value)}
+            />
+            <div
+              style={{
+                margin: "0 0.5rem 0.5rem 1.5rem",
+                background: "#fff",
+                borderRadius: "50%",
+              }}
+            >
+              <Avatar
+                alt={userDetails ? userDetails.email : "Test"}
+                src={userDetails && userDetails.pictureUrl}
+              />
+            </div>
+            <IconButton onClick={handleClick}>
+              <ExpandMoreIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={menuOpen}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </div>
+      </StyledAppBarMobile>
+    </div>
+  );
+};
+
+export default SmallDevicesSidebar;
--- frontend/src/components/Sidebar/style.js
+++ frontend/src/components/Sidebar/style.js
@@ -3,14 +3,28 @@ import styled from "styled-components";
 import { Brand2Svg } from "../../assets/icons";
 
 export const StyledAppBar = styled(AppBar)`
-  padding-left: ${(props) => (props.collapsed ? "4%" : "15%")};
+  padding-left: ${(props) => (props.collapsed ? "70px" : "230px")};
   transition: all 0.5s;
   height: 3.5rem;
 `;
 
 export const StyledDrawer = styled(Drawer)`
   .MuiPaper-root {
-    width: ${(props) => (props.open ? "15%" : "4%")};
+    width: ${(props) => (props.open ? "230px" : "70px")};
+    background-color: #744fc2;
+    color: #fff;
+    transition: all 0.3s;
+    overflow-x: hidden;
+  }
+`;
+
+export const StyledAppBarMobile = styled(AppBar)`
+  transition: all 0.5s;
+  height: 3.5rem;
+`;
+
+export const StyledDrawerMobile = styled(Drawer)`
+  .MuiPaper-root {
     background-color: #744fc2;
     color: #fff;
     transition: all 0.3s;
@@ -47,6 +61,29 @@ export const BrandDrawer = () => {
         alt="brandimage"
       />
       <h3>EXP TRACKER</h3>
+    </div>
+  );
+};
+
+export const BrandDrawerMobile = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src={Brand2Svg}
+        style={{
+          display: "block",
+          margin: "1rem 0",
+          width: "35px",
+        }}
+        alt="brandimage"
+      />
+      <h4 style={{ marginLeft: "-10px" }}>EXP TRACKER</h4>
     </div>
   );
 };
