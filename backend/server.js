c797bf0dfa75dc25f582256079d058b27a97538c
DhrunitSixberries <dhrunit@sixberries.com>
2022-01-28 10:13:58 +0530

    Added some configuration

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ import dashboardRoutes from "./routes/dashboardRoutes.js";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 import { resetBalance } from "./utils/cron.js";
+import { dirname } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 connectDB();
@@ -24,16 +30,23 @@ app.use("/api/wallet", walletRoutes);
 app.use("/api/transaction", transactionRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
-app.use(errorHandler);
-app.use(notFound);
-
 schedule.scheduleJob("0 0 * * *", async function () {
   resetBalance();
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(errorHandler);
+app.use(notFound);
 
 const PORT = process.env.PORT || 5000;
 
