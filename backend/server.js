9e9940264c738839ea4f8a2a1c12893cd23e9a85
DhrunitSixberries <dhrunit@sixberries.com>
2022-01-28 11:16:11 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,9 @@ app.disable("etag");
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.get("api/test", (req, res) => {
+  res.send("API is running....");
+});
 app.use("/api/users", userRoutes);
 app.use("/api/wallet", walletRoutes);
 app.use("/api/transaction", transactionRoutes);
