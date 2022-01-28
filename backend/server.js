041aa9d8056c26de804471de36d565a040dac967
DhrunitSixberries <dhrunit@sixberries.com>
2022-01-28 10:42:02 +0530

    setup changes

--- backend/server.js
+++ backend/server.js
@@ -34,16 +34,9 @@ schedule.scheduleJob("0 0 * * *", async function () {
   resetBalance();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
 app.use(errorHandler);
 app.use(notFound);
